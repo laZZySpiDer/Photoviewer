aad05cfabdb1e09e2b0d80d8d97397f76c57f225
Aditya <adityapandya7001@gmail.com>
2018-01-20 12:40:50 +0530

    Add project files.

---
+++ laZZyViewer/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace laZZyViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menulist1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnclose = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnzoomout = new System.Windows.Forms.Button();
+            this.btnzoomin = new System.Windows.Forms.Button();
+            this.btnrotateleft = new System.Windows.Forms.Button();
+            this.btnrotateright = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menulist1,
+            this.helpToolStripMenuItem});
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // menulist1
+            // 
+            this.menulist1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menulist1.ForeColor = System.Drawing.Color.Black;
+            this.menulist1.Name = "menulist1";
+            resources.ApplyResources(this.menulist1, "menulist1");
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            resources.ApplyResources(this.newToolStripMenuItem, "newToolStripMenuItem");
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            resources.ApplyResources(this.exitToolStripMenuItem, "exitToolStripMenuItem");
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            resources.ApplyResources(this.helpToolStripMenuItem, "helpToolStripMenuItem");
+            // 
+            // btnclose
+            // 
+            resources.ApplyResources(this.btnclose, "btnclose");
+            this.btnclose.BackColor = System.Drawing.Color.Red;
+            this.btnclose.ForeColor = System.Drawing.Color.White;
+            this.btnclose.Name = "btnclose";
+            this.btnclose.UseVisualStyleBackColor = false;
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.ForeColor = System.Drawing.Color.Transparent;
+            this.panel1.Name = "panel1";
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.tableLayoutPanel1.Controls.Add(this.btnrotateright, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnzoomin, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnzoomout, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnrotateleft, 0, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // btnzoomout
+            // 
+            resources.ApplyResources(this.btnzoomout, "btnzoomout");
+            this.btnzoomout.Name = "btnzoomout";
+            this.btnzoomout.UseVisualStyleBackColor = true;
+            this.btnzoomout.Click += new System.EventHandler(this.btnzoomout_Click);
+            // 
+            // btnzoomin
+            // 
+            resources.ApplyResources(this.btnzoomin, "btnzoomin");
+            this.btnzoomin.Name = "btnzoomin";
+            this.btnzoomin.UseVisualStyleBackColor = true;
+            this.btnzoomin.Click += new System.EventHandler(this.btnzoomin_Click);
+            // 
+            // btnrotateleft
+            // 
+            resources.ApplyResources(this.btnrotateleft, "btnrotateleft");
+            this.btnrotateleft.Name = "btnrotateleft";
+            this.btnrotateleft.UseVisualStyleBackColor = true;
+            this.btnrotateleft.Click += new System.EventHandler(this.btnrotateleft_Click);
+            // 
+            // btnrotateright
+            // 
+            resources.ApplyResources(this.btnrotateright, "btnrotateright");
+            this.btnrotateright.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnrotateright.Name = "btnrotateright";
+            this.btnrotateright.UseVisualStyleBackColor = false;
+            this.btnrotateright.Click += new System.EventHandler(this.btnrotateright_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnclose);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menulist1;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Button btnclose;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnzoomout;
+        private System.Windows.Forms.Button btnzoomin;
+        private System.Windows.Forms.Button btnrotateright;
+        private System.Windows.Forms.Button btnrotateleft;
+    }
+}
+
---
+++ laZZyViewer/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace laZZyViewer
+{
+    public partial class Form1 : Form
+    {
+        String filepath;
+        bool imageOpened = false;
+        Bitmap bitmap1;
+        public Form1()
+        {
+            InitializeComponent();
+            menulist1.Visible = false;
+            helpToolStripMenuItem.Visible = false;
+            panel1.Visible = true;
+            tableLayoutPanel1.Visible = false;
+            hintforbutton();
+            
+            
+        }
+        public void hintforbutton()
+        {
+            ToolTip obj1 = new ToolTip();
+            obj1.SetToolTip(btnrotateleft,"Rotate Left");
+            obj1.SetToolTip(btnrotateright,"Rotate Right");
+            obj1.SetToolTip(btnzoomin,"Zoom In");
+            obj1.SetToolTip(btnzoomout, "Zoom Out");
+        }
+        public void opennewImage()
+        {
+           //a function to open the image.
+           //it is called twice. When NEW is clicked from menu strip
+           //or when the picture box is clicked once
+            OpenFileDialog browseFolder = new OpenFileDialog();
+            DialogResult dialogresult = browseFolder.ShowDialog();
+            if (dialogresult == DialogResult.OK)
+            {
+                filepath = browseFolder.ToString();
+                PictureBox pic_new = new PictureBox();
+                pictureBox1.Image = Image.FromFile(browseFolder.FileName);
+                bitmap1 = (Bitmap)pictureBox1.Image;
+                imageOpened = true;
+                menulist1.Visible = true;
+                helpToolStripMenuItem.Visible = true;
+                tableLayoutPanel1.Visible = true;
+
+            }
+        }
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            opennewImage();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if(imageOpened == false)
+            {
+                opennewImage();
+            }            
+        }
+
+        private void btnzoomout_Click(object sender, EventArgs e)
+        {
+
+            pictureBox1.Top = (int)(pictureBox1.Top + (pictureBox1.Height * 0.025));
+            pictureBox1.Left = (int)(pictureBox1.Left + (pictureBox1.Width * 0.025));
+            pictureBox1.Height = (int)(pictureBox1.Height - (pictureBox1.Height * 0.05));
+            pictureBox1.Width = (int)(pictureBox1.Width - (pictureBox1.Width * 0.05));
+        }
+
+        private void btnrotateright_Click(object sender, EventArgs e)
+        {
+            bitmap1.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            pictureBox1.Image = bitmap1;
+        }
+
+        private void btnrotateleft_Click(object sender, EventArgs e)
+        {
+            bitmap1.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            pictureBox1.Image = bitmap1;
+           
+            
+        }
+
+        private void btnzoomin_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Top = (int)(pictureBox1.Top - (pictureBox1.Height * 0.025));
+            pictureBox1.Left = (int)(pictureBox1.Left - (pictureBox1.Width * 0.025));
+            pictureBox1.Height = (int)(pictureBox1.Height + (pictureBox1.Height * 0.05));
+            pictureBox1.Width = (int)(pictureBox1.Width + (pictureBox1.Width * 0.05));
+        }
+    }
+}
